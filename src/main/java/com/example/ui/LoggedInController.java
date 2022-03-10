9300e4d8276a2d9806a71cb47f42957b45d1f7fa
filippo <filippo-ceradini>
2022-03-10 11:46:39 +0100

    added 4 seach pages on the logged in page

--- src/main/java/com/example/ui/LoggedInController.java
+++ src/main/java/com/example/ui/LoggedInController.java
@@ -16,11 +16,24 @@ public class LoggedInController implements Initializable {
     private Label welcome_label;
     @FXML
     private Button logout_button;
+    @FXML
+    private Button go_to_show_children_button;
+    @FXML
+    private Button go_to_show_parents_button;
+    @FXML
+    private Button go_to_show_employees_button;
+    @FXML
+    private Button go_to_show_rooms_button;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         //Lambda instead of eventhandler
         logout_button.setOnAction(event -> Utils.changeScene(event, "login-view.fxml", "Log in", null));
+        go_to_show_children_button.setOnAction(event -> Utils.changeScene(event, "show-children-view.fxml", "Show Children", null));
+        go_to_show_parents_button.setOnAction(event -> Utils.changeScene(event, "show-parents-view.fxml", "Show Parents", null));
+        go_to_show_employees_button.setOnAction(event -> Utils.changeScene(event, "show-employees-view.fxml", "Show Employees", null));
+        go_to_show_rooms_button.setOnAction(event -> Utils.changeScene(event, "show-rooms-view.fxml", "Show Rooms", null));
+
     }
 
     public void setUserInformation(String username) {
---
+++ src/main/java/com/example/ui/ShowEmployeesController.java
@@ -0,0 +1,19 @@
+package com.example.ui;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ShowEmployeesController implements Initializable {
+    @FXML
+    private Button back_to_loggedin_button;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        back_to_loggedin_button.setOnAction(event -> Utils.changeScene(event, "loggedin-view.fxml", "Main Menu", null));
+    }
+}
