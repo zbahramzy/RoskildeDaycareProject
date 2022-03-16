51ff6da955c5f83d15f61621fe4d10ab8c776f2e
filippo <filippo-ceradini>
2022-03-16 12:39:11 +0100

    Created setters/getters for child_date_of_birth

--- src/main/java/com/example/ui/SearchObjects.java
+++ src/main/java/com/example/ui/SearchObjects.java
@@ -40,98 +40,84 @@ public class SearchObjects {
         this.employee_title = employee_title;
     }
 
+    //Getters
+    public LocalDate getChild_date_of_birth() {
+        return child_date_of_birth;
+    }
     public Integer getClass_id() {
         return class_id;
     }
-
     public Integer getParent_id() {
         return parent_id;
     }
-
     public Integer getEmployee_id() {
         return employee_id;
     }
-
     public String getChild_firstname() {
         return child_firstname;
     }
-
     public String getChild_lastname() {
         return child_lastname;
     }
-
     public String getParent_firstname() {
         return parent_firstname;
     }
-
     public String getParent_lastname() {
         return parent_lastname;
     }
-
     public String getParent_phone() {
         return parent_phone;
     }
-
     public String getEmployee_firstname() {
         return employee_firstname;
     }
-
     public String getEmployee_lastname() {
         return employee_lastname;
     }
-
     public String getEmployee_phone() {
         return employee_phone;
     }
-
     public String getEmployee_title() {
         return employee_title;
     }
 
+    //Setters
+    public void setChild_date_of_birth(LocalDate child_date_of_birth) {
+        this.child_date_of_birth = child_date_of_birth;
+    }
     public void setClass_id(Integer class_id) {
         this.class_id = class_id;
     }
-
     public void setParent_id(Integer parent_id) {
         this.parent_id = parent_id;
     }
-
     public void setEmployee_id(Integer employee_id) {
         this.employee_id = employee_id;
     }
-
     public void setChild_firstname(String child_firstname) {
         this.child_firstname = child_firstname;
     }
-
     public void setChild_lastname(String child_lastname) {
         this.child_lastname = child_lastname;
     }
-
     public void setParent_firstname(String parent_firstname) {
         this.parent_firstname = parent_firstname;
     }
-
     public void setParent_lastname(String parent_lastname) {
         this.parent_lastname = parent_lastname;
     }
-
     public void setParent_phone(String parent_phone) {
         this.parent_phone = parent_phone;
     }
-
     public void setEmployee_firstname(String employee_firstname) {
         this.employee_firstname = employee_firstname;
     }
-
     public void setEmployee_lastname(String employee_lastname) {
         this.employee_lastname = employee_lastname;
     }
-
     public void setEmployee_phone(String employee_phone) {
         this.employee_phone = employee_phone;
     }
-
     public void setEmployee_title(String employee_title) {
         this.employee_title = employee_title;
     }
